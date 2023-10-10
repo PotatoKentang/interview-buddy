e4aec0c9902378a71a30868998bd9110767f64fc
jeffjuann <jeffjuann@gmail.com>
2023-10-10 20:43:43 +0700

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,14 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+import { GluestackUIProvider, config } from "@gluestack-ui/themed";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import InterviewHeader from "../components/interview-header";
+export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "index",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,8 +16,16 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SatoshiBlack: require("../assets/fonts/Satoshi-Black.otf"),
+    SatoshiBlackItalic: require("../assets/fonts/Satoshi-BlackItalic.otf"),
+    SatoshiBold: require("../assets/fonts/Satoshi-Bold.otf"),
+    SatoshiBoldItalic: require("../assets/fonts/Satoshi-BoldItalic.otf"),
+    SatoshiLight: require("../assets/fonts/Satoshi-Light.otf"),
+    SatoshiLightItalic: require("../assets/fonts/Satoshi-LightItalic.otf"),
+    SatoshiMedium: require("../assets/fonts/Satoshi-Medium.otf"),
+    SatoshiMediumItalic: require("../assets/fonts/Satoshi-MediumItalic.otf"),
+    // ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -43,14 +47,54 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider config={config.theme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="interview/[id]"
+          options={{
+            header: () => <InterviewHeader type="home" title="Interview"/>,
+          }}
+        />
+        <Stack.Screen
+          name="interview/interview-type"
+          options={{
+            header: () => <InterviewHeader type="back" title="Interview Type"/>,
+          }}
+        />
+        <Stack.Screen
+          name="interview/interview-profile"
+          options={{
+            header: ()=><InterviewHeader type="back" title="Setup Interview Profile"/>,
+          }}
+        />
+        <Stack.Screen
+          name="interview/recent-interviews"
+          options={{
+            header: ()=><InterviewHeader type="back" title="Recent Interviews"/>,
+          }}
+        />
+        <Stack.Screen
+          name="interview/bulk/index"
+          options={{
+            header: ()=><InterviewHeader type="back" title="List Of Questions"/>,
+          }}
+        />
+        <Stack.Screen
+          name="interview/bulk/[id]"
+          options={{
+            headerShown:false,
+            presentation: 'modal',
+            gestureEnabled: true
+          }}
+        />
       </Stack>
-    </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
--- temp/app/[...missing].tsx
+++ temp/app/[...missing].tsx
@@ -1,14 +1,15 @@
-import { Link, Stack } from "expo-router";
-import { StyleSheet } from "react-native";
-import React from "react";
-import { Text, View } from "react-native";
+import { Link, Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
+
+import { Text, View } from '../components/Themed';
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Oops!" }} />
+      <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn&apos;t exist.</Text>
+        <Text style={styles.title}>This screen doesn't exist.</Text>
+
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
@@ -19,21 +20,21 @@ export default function NotFoundScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     flex: 1,
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   link: {
     marginTop: 15,
     paddingVertical: 15,
   },
   linkText: {
-    color: "#2e78b7",
     fontSize: 14,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+    color: '#2e78b7',
   },
 });
